b73d17e43e61361102987cdbb5fde10c72e7c31b
Yehor <augortv@gmail.com>
2024-02-06 19:50:54 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+
+    // sidenav
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems, {
+            // edge: 'right'
+        });
+
+
+
+
+
+
+
+
+        
+    });
